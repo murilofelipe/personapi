36b18ef72b7d40f80d2c5c95e0d8b95877747d3a
murilofelipe <murilosilva.felipe@gmail.com>
2022-01-17 18:01:49 -0400

    Adição de TDD

---
+++ src/test/java/com/dio/personapi/services/PersonServiceTest.java
@@ -0,0 +1,54 @@
+package com.dio.personapi.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.dio.personapi.dto.mapper.PersonMapper;
+import com.dio.personapi.dto.request.PersonDTO;
+import com.dio.personapi.dto.response.MessageResponseDTO;
+import com.dio.personapi.entity.Person;
+import com.dio.personapi.repository.PersonRepository;
+import com.dio.personapi.service.PersonService;
+
+import static com.dio.personapi.utils.PersonUtils.createFakeDTO;
+import static com.dio.personapi.utils.PersonUtils.createFakeEntity;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class PersonServiceTest {
+
+    @Mock
+    private PersonRepository personRepository;
+
+    @Mock
+    private PersonMapper personMapper;
+
+    @InjectMocks
+    private PersonService personService;
+
+    @Test
+    void testGivenPersonDTOThenReturnSuccessSavedMessage() {
+        PersonDTO personDTO = createFakeDTO();
+        Person expectedSavedPerson = createFakeEntity();
+
+        when(personMapper.toModel(personDTO)).thenReturn(expectedSavedPerson);
+        when(personRepository.save(any(Person.class))).thenReturn(expectedSavedPerson);
+
+        MessageResponseDTO expectedSuccessMessage = createExpectedSuccessMessage(expectedSavedPerson.getId());
+        MessageResponseDTO successMessage = personService.createPerson(personDTO);
+
+        assertEquals(expectedSuccessMessage, successMessage);
+    }
+
+    private MessageResponseDTO createExpectedSuccessMessage(Long savedPersonId) {
+        return MessageResponseDTO.builder()
+                .message("Person successfully created with ID " + savedPersonId)
+                .build();
+    }
+
+}
