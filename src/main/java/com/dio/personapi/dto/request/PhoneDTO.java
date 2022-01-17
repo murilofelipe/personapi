6441588367e97d9534ab89812b088b1bcd89f944
murilofelipe <murilosilva.felipe@gmail.com>
2022-01-17 13:41:14 -0400

    Adicionado classes DTO

---
+++ src/main/java/com/dio/personapi/dto/request/PhoneDTO.java
@@ -0,0 +1,29 @@
+package com.dio.personapi.dto.request;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import com.dio.personapi.enums.PhoneType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PhoneDTO {
+
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PhoneType type;
+
+    @NotEmpty
+    @Size(min = 13, max = 14)
+    private String number;
+}
