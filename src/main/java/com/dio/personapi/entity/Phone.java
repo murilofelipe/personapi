775f6b8e085135292d391e25371a8aa695367ab4
murilofelipe <murilosilva.felipe@gmail.com>
2022-01-17 12:47:51 -0400

    Adição de classes para persistência de dados

---
+++ src/main/java/com/dio/personapi/entity/Phone.java
@@ -0,0 +1,35 @@
+package com.dio.personapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.dio.personapi.enums.PhoneType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false, unique = true)
+    private String number;
+}
